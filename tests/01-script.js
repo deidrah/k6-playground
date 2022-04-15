b18a7b5e879df1251b93fed2c8dde8af133219e5
Deidrah <dominikastasik93@gmail.com>
2022-04-15 17:40:07 +0200

    rampup rumpdown

--- tests/01-script.js
+++ tests/01-script.js
@@ -1,8 +1,10 @@
 import http from 'k6/http';
 
 export let options = {
-  vus: 10,
-  duration: '10s'
+  stages: [
+    {duration: '10s', target: 5}, //5 virtual users for 10s
+    {duration: '20s', target: 25}
+  ]
 }
 
 export default function() {
